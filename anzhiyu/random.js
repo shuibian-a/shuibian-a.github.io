068f8bc09f968539e019967f508f95c789de3ab4
shuibian-a <buyixiaoshen@163.com>
2023-04-22 12:43:27 +0800

    Site updated: 2023-04-22 12:43:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/04/22/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/04/15/华为HarmonyOS（鸿蒙系统）/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
